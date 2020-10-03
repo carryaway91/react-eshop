7acbcf34443a1510439e32842fead6c785fb2049
carryaway91 <carryaway91@gmail.com>
2020-10-03 17:41:37 +0200

    Updates

---
+++ precache-manifest.b09760cd4e2a5878582369aef39e978e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09b0cc8eee00ad37c8fa1f213d8bdf3c",
+    "url": "/react-eshop/index.html"
+  },
+  {
+    "revision": "f53a33cb5fa797704606",
+    "url": "/react-eshop/static/css/main.8ff07546.chunk.css"
+  },
+  {
+    "revision": "ac4e855da35bcb8b0745",
+    "url": "/react-eshop/static/js/2.5c5e0fcb.chunk.js"
+  },
+  {
+    "revision": "dd207babaeb913476be46155cc504ef5",
+    "url": "/react-eshop/static/js/2.5c5e0fcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f53a33cb5fa797704606",
+    "url": "/react-eshop/static/js/main.a3e4fb5b.chunk.js"
+  },
+  {
+    "revision": "e9dbcd0f8c19f058e237",
+    "url": "/react-eshop/static/js/runtime-main.249e4937.js"
+  }
+]);
