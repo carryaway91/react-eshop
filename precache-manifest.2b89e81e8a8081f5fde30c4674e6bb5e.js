5f0df7090c79ebfbd6c408e70cae52cf11151345
carryaway91 <carryaway91@gmail.com>
2021-07-04 14:18:16 +0200

    Updates

---
+++ precache-manifest.2b89e81e8a8081f5fde30c4674e6bb5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a1ee80f69187e94057fbd2f54ef602c",
+    "url": "/react-eshop/index.html"
+  },
+  {
+    "revision": "d7293c8781fd2826cfd2",
+    "url": "/react-eshop/static/css/main.34b0cc2c.chunk.css"
+  },
+  {
+    "revision": "b68df342ae0cf610f091",
+    "url": "/react-eshop/static/js/2.fe437a9c.chunk.js"
+  },
+  {
+    "revision": "dd207babaeb913476be46155cc504ef5",
+    "url": "/react-eshop/static/js/2.fe437a9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7293c8781fd2826cfd2",
+    "url": "/react-eshop/static/js/main.98bf4a8e.chunk.js"
+  },
+  {
+    "revision": "e9dbcd0f8c19f058e237",
+    "url": "/react-eshop/static/js/runtime-main.249e4937.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-eshop/precache-manifest.cd1959887f8f99046741f87fc35d957c.js"
+  "/react-eshop/precache-manifest.2b89e81e8a8081f5fde30c4674e6bb5e.js"
 );
 
 self.addEventListener('message', (event) => {
